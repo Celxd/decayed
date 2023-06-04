36d844ad79c195c451ea4eafe77abb77dda68899
Gib <celxd250606@gmail.com>
2023-06-04 21:59:08 +0700

    Temp pickup prompt

--- Assets/Scripts/ItemObject.cs
+++ Assets/Scripts/ItemObject.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ItemObject : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] public Item item;
 }
