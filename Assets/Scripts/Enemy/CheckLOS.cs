0e13d5174842d654955368b2ed909e45020ed9dc
Gib <celxd250606@gmail.com>
2023-06-13 16:14:42 +0700

    Check LOS
    LOS (Line Of Sight)

--- Assets/Scripts/Enemy/CheckLOS.cs
+++ Assets/Scripts/Enemy/CheckLOS.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SphereCollider))]
 public class CheckLOS : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //fyi, LOS stands for "Line Of Sight"
+
+    SphereCollider m_SphereCollider;
+    float m_Fov = 90f;
+    LayerMask m_LOSLayer;
+
+    public delegate void GainSightEvent(Transform target);
+    public event GainSightEvent OnGainSight;
+    public delegate void LoseSightEvent(Transform target);
+    public event LoseSightEvent OnLoseSight;
+
+    Coroutine m_CheckLOSCoroutine;
+
+    private void Awake()
     {
-        
+        m_SphereCollider = GetComponent<SphereCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!CheckLOSBool(other.transform))
+        {
+
+        }
+    }
+
+    private bool CheckLOSBool(Transform target)
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        float dotProduct = Vector3.Dot(transform.forward, direction);
+        if (dotProduct > Mathf.Cos(m_Fov)) {
+            if (Physics.Raycast(transform.position, direction, out RaycastHit hit, m_SphereCollider.radius, m_LOSLayer))
+            {
+                OnGainSight?.Invoke(target);
+                return true;
+            }
+        }
+        return false;
     }
 }
